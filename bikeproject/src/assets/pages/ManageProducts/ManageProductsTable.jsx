a0f6f4d341b93f82dd3e8cec8093ed8aa55823bd
MD Abrar Yasir <abraryasirrafi@gmail.com>
2023-11-26 04:31:47 -0800

    working on manage product button

--- bikeproject/src/assets/pages/ManageProducts/ManageProductsTable.jsx
+++ bikeproject/src/assets/pages/ManageProducts/ManageProductsTable.jsx
@@ -1,4 +1,7 @@
 import { Link, useLoaderData } from "react-router-dom";
+import { BiCommentDetail } from "react-icons/bi";
+import { MdDelete } from "react-icons/md";
+import { RxUpdate } from "react-icons/rx";
 function ManageProductsTable() {
   const vehicles = useLoaderData();
   console.log(vehicles);
@@ -25,20 +28,20 @@ function ManageProductsTable() {
                     type="button"
                     class="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
                   >
-                    D
+                    <BiCommentDetail size={15}></BiCommentDetail>
                   </button>
                 </Link>
                 <button
                   type="button"
                   class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
                 >
-                  X
+                  <MdDelete size={15}></MdDelete>
                 </button>
                 <button
                   type="button"
                   class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
                 >
-                  U
+                  <RxUpdate size={15}></RxUpdate>
                 </button>
               </td>
             </tr>
